b80b726b2f2c51bebcb9a32abcfe8ebf828b5937
Arjun Jariwala <arjunjariwala1994@gmail.com>
2019-05-05 15:44:45 -0400

    Json and printing to UI

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EmpDetails()
+        {
+            //Creating List
+            List<Employee> ObjEmp = new List<Employee>()
+            {
+        //Adding records to list
+        new Employee {Id=1,Name="Arjun Jariwala" },
+        new Employee {Id=2,Name="Madhavi Jariwala" }
+            };
+            //return list as Json
+            return Json(ObjEmp);
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
